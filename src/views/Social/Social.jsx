b340215e4912e0c64d879d873d2992832b1194a3
Manya Malik <manyamalik3112@gmail.com>
2026-08-01 22:28:44 +0530

    first commit

--- src/views/Social/Social.jsx
+++ src/views/Social/Social.jsx
@@ -6,21 +6,49 @@ import linkedinlogo from "../../assets/images/SocialMedia/linkedinLogo.svg";
 import instalogo from "../../assets/images/SocialMedia/instaLogo.svg";
 import styles from "./styles/Social.module.scss";
 import { ComponentLoading } from "../../microInteraction";
+import { api } from "../../services";
+import BlogCard from "../../components/BlogCard/BlogCard";
 
 const Social = () => {
+  const [blogs, setBlogs] = useState([]);
+  const [loadingBlogs, setLoadingBlogs] = useState(true);
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      try {
+        const response = await api.get("/api/blog/getBlog");
 
+        if (response.status === 200) {
+          const processedBlogs = (response.data.blogs || [])
+            .filter(
+              (blog) =>
+                blog.visibility !== "private" &&
+                blog.approval !== false
+            )
+            .sort((a, b) => new Date(b.date) - new Date(a.date));
+
+          setBlogs(processedBlogs);
+        }
+      } catch (err) {
+        console.error("Failed to fetch blogs:", err);
+      } finally {
+        setLoadingBlogs(false);
+      }
+    };
+
+    fetchBlogs();
+  }, []);
 
   return (
-
     <div className={styles.socialMcontainer}>
       <div className={styles.text}>
         <div className={styles.circleCenter}></div>
         <div className={styles.content}>
-          <p>Welcome to the social media page of</p>
+          Welcome to the social media page of <br />
           <div className={styles.fed}>
             <div className={styles.box} id={styles.box1}>
               <img
@@ -45,6 +73,7 @@ const Social = () => {
               />
             </div>
           </div>
+          <br />
         </div>
       </div>
       <div className={styles.socialMedia}>
@@ -73,8 +102,26 @@ const Social = () => {
           </div>
         </div>
       </div>
-    </div>
 
+      <div className={styles.blogSection}>
+        <h2>Latest Blogs</h2>
+
+        {loadingBlogs ? (
+          <ComponentLoading />
+        ) : (
+          <div className={styles.blogGrid}>
+            {blogs.slice(0, 3).map((blog) => (
+              <BlogCard
+                key={blog.id || blog._id}
+                data={blog}
+                cardType="recent"
+                hideDescription={false}
+              />
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
   );
 };
 
